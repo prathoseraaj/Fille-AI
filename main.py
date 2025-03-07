b2ba212b71a92fffc6c1bf76cbbf4d2b1b816672
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-03-07 16:03:41 +0530

    fix: normal chatbot

--- main.py
+++ main.py
@@ -1,39 +1,21 @@
 import os
 import requests
-import pandas as pd
-from datasets import load_dataset
 from fastapi import FastAPI
 from dotenv import load_dotenv
 
 load_dotenv()
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 
-dataset = load_dataset("altaidevorg/women-health-mini")
-df = pd.DataFrame(dataset["train"])
-
-
 app = FastAPI()
 
 GROQ_API_URL = "https://api.groq.com/openai/v1/chat/completions"
-
-def search_csv(questions):
-    for _, row in df.iterrows():
-        if questions.lower() in row['Question'].lower():
-            return row['answer']
-    return None
 
 @app.post("/chat/")
 def chat_with_bot(user_query:dict):
     prompt = user_query.get("message", "")
 
     if not prompt :
-        return "Message is required!"
-    
-    csv_answer = search_csv(prompt)
-    if csv_answer:
-        return {
-            "response": csv_answer
-        }
+        return "Prompt is required!"
     
     headers = {
         "Authorization": f"Bearer {GROQ_API_KEY}",
