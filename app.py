28dd878c49ba2f27ce68ad7b4e878079936b0e9d
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-03-07 14:22:23 +0530

    feat: To check the prompt

--- app.py
+++ app.py
@@ -9,3 +9,11 @@ GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 app = FastAPI()
 
 GROQ_API_URL = "https://api.groq.com/v1/chat/completions"
+
+@app.post("/chat/")
+def chat_with_bot(user_query:dict):
+    prompt = user_query.get("message", "")
+
+    if not prompt :
+        return "Prompt is required!"
+      
