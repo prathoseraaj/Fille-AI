a34d1efb5d074e2dc30c690448f9ac9388844d6c
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-04-09 15:45:13 +0530

    fix js

--- server/server.js
+++ server/server.js
@@ -232,5 +232,5 @@ io.on('connection', (socket) => {
 // Start server
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`)
 });
