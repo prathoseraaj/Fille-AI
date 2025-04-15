9dd9088f15075267bd6d7f84e4cff961638d5720
Prathoseraaj V <prathoseraaj0312@gmail.com>
2025-04-15 11:06:47 +0530

    fix: status bar

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,5 +1,16 @@
 import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar"; 
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <>
+      <StatusBar style="light" hidden={true} />
+      <Stack
+        screenOptions={{
+          headerShown: false, 
+          statusBarHidden: true 
+        }}
+      />
+    </>
+  );
 }
